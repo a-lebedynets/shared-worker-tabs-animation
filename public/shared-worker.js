efcae9b7b869cd1dced0c491f3f14b75c2fe1dbd
Alex Lebed <sanylebed@gmail.com>
2020-02-07 05:53:00 +0200

    working example

--- public/shared-worker.js
+++ public/shared-worker.js
@@ -1,5 +1,7 @@
 let connections = [];
 let increment = 0;
+let animationId;
+let isLast = false;
 
 onconnect = function (e) {
     e.source.start();
@@ -15,16 +17,51 @@ onconnect = function (e) {
         connection: port,
     });
 
-    e.source.onmessage = ({ data: message }) => {
-        console.log(message);
+    port.postMessage({ key: 'connect', increment });
 
-        switch (message) {
-            case 'is-first':
-                port.postMessage({ key: 'is-first', value: increment === 1 });
+    e.source.onmessage = ({ data }) => {
+        console.log(data);
+
+        switch (data.key) {
+
+            // case 'connect':
+            //     port.postMessage({ key: 'connect', increment });
+            //     break;
+
+            case 'start':
+                if (animationId) return;
+                animationId = data.increment;
+                connections[increment - 1].connection.postMessage({ key: 'start', increment: data.increment });
                 break;
 
             case 'end':
-                port.postMessage({ key: 'end', value: increment === 1 });
+                // if (animationId === data.increment) {
+
+                console.log(increment, animationId);
+
+                if (increment > data.increment && !isLast) {
+                    animationId++;
+                    connections[data.increment].connection.postMessage({ key: 'start', increment: animationId });
+                } else {
+                    if (animationId > 0) {
+                        isLast = true;
+                        animationId--;
+                        if (connections[animationId])
+                            connections[animationId].connection.postMessage({ key: 'end', increment: animationId });
+                    } else {
+                        isLast = false;
+                        animationId = 1;
+                        connections[data.increment].connection.postMessage({ key: 'start', increment: animationId });
+                    }
+
+                }
+                // else {
+                //     port.postMessage({ key: 'end', increment });
+                // }
+                // } else {
+
+                // }
+                // 
                 break;
 
             default:
